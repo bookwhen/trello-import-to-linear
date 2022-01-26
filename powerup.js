0d70bf538c31e1472f5d88a5c19ea8acd0ddfddb
Henry Blyth <blyth.henry@gmail.com>
2022-01-26 11:01:56 +0000

    Try no icon in card button

--- powerup.js
+++ powerup.js
@@ -3,8 +3,6 @@ var TrelloPowerUp = /** @type {ExtendedWindow} */(window).TrelloPowerUp;
 
 console.log('henlo, am client');
 
-var icon = 'https://henryblyth.com/images/watercolour-circle-light.svg';
-
 window.Promise = TrelloPowerUp.Promise; // for browser compatibility
 
 TrelloPowerUp.initialize(makeConfig());
@@ -49,44 +47,10 @@ function makeCopyToLinearCardButton(card) {
   linearUrl.searchParams.set('labels', labels.join(','));
 
   return {
-    icon: icon,
+    icon: null,
     text: 'Open in Linear',
     condition: 'edit',
     url: linearUrl.toString(),
     target: '_blank'
   };
-}
-
-/**
- * @param {Trello} t
- * @returns {Promise<TrelloCard[]>}
- */
-function getAllBoardCards(t) {
-  return t.lists('id', 'name', 'cards').then(function (lists) {
-    console.log(lists.map(l => `${l.name} (${l.cards.length})`));
-    return lists.map(list => list.cards).flat();
-  });
-}
-
-/**
- * @param {Trello} t
- * @returns {Promise<TrelloPopupItem[]>}
- */
-function getUpNextItems(t) {
-  return new Promise(function (resolve, reject) {
-    console.log('popup items, return promise');
-
-    console.log('fetching lists…');
-    return getAllBoardCards(t).then(function (cards) {
-      var cardItems = cards.map(function (card) {
-        return {
-          text: card.name,
-          callback: function (t, opts) {
-            console.log('chosen', card.name, card.id);
-          }
-        };
-      });
-      return resolve(cardItems);
-    });
-  });
 }
